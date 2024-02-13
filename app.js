20f269adefd1bf5d1c3d18f837a3d70725562b55
Negin <asnavandinegin@gmail.com>
2024-02-14 01:17:28 +0330

    circle

---
+++ app.js
@@ -0,0 +1,9 @@
+const board = document.querySelector('.fullboard');
+const peg = document.querySelector('.pegs');
+
+for(var i = 0 ; i < 44 ; i ++){
+
+    board.innerHTML += `<div class = "circle"></div>`
+
+    peg.innerHTML += `<div class="pegcircle"></div>`
+}
