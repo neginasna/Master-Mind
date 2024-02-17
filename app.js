118cce24775e4a9f029662ac57c6dd2e58e7f5e5
Negin <asnavandinegin@gmail.com>
2024-02-17 14:56:14 +0330

    color JS

--- app.js
+++ app.js
@@ -8,6 +8,36 @@ for(var i = 0 ; i < 44 ; i ++){
     peg.innerHTML += `<div class="pegcircle"></div>`
 }
 
+$(document).ready(function(){
+
+    $(".color").each(function(){
+        let color = $(this).attr("id");
+        $(this).css("background-color", color);
+    });
+
+
+        $(".color").click(function(){
+            let color = $(this).attr("id");
+            currentcolor = color;
+    
+             $(".currentcolor").css("background-color", color)
+
+         });
+
+
+
+         $(".circle").click(function(){
+            let color = $(this).attr("id");
+            $(this).css("background-color", currentcolor)
+
+         });
+
+
+
+    })
+
+
+
 
 
 
