b0692db874834a5805c800bd9a78ffa0433099f5
Negin <asnavandinegin@gmail.com>
2024-02-29 00:33:51 +0330

    running the game

--- app.js
+++ app.js
@@ -12,7 +12,6 @@
 
 
 $(document).ready(function () {
-
     var current = "white";
     var currentBoardCircles = ["board41", "board42", "board43", "board44"];
     var currentPegCircles = ["peg41", "peg42", "peg43", "peg44"];
@@ -20,6 +19,9 @@ $(document).ready(function () {
     var possibleColors = ["blue", "green", "red", "yellow", "orange", "pink"];
 
     var circle1Color, circle2Color, circle3Color, circle4Color;
+
+    let currentColor = undefined;
+    const userChoices = [];
 
 
     var colors = {
@@ -32,6 +34,31 @@ $(document).ready(function () {
     }
 
     const currentSecrets = [];
+
+    function continueGame(targetNode) {
+        const id = targetNode.id;
+        const nodeNumber = id.substring(5);
+        const targetRow = Math.ceil(nodeNumber/4);
+        console.log(targetRow, nodeNumber, nodeNumber % 4, currentColor);
+        $(targetNode).css('background-color', currentColor);
+
+        const targetIndex = nodeNumber % 4;
+
+        if (currentColor) {
+            if (targetIndex === 0) {
+                userChoices[3] = currentColor;
+            } else {
+                userChoices[targetIndex - 1] = currentColor;
+            }
+            console.log(currentSecrets);
+            console.log(userChoices);
+        }
+
+
+
+        
+    }
+
     $(".secretcolor").each(function () {
 
         const secret = possibleColors[Math.floor(Math.random() * 6)];
@@ -42,14 +69,20 @@ $(document).ready(function () {
     });
 
     for (let i = 1; i <= 44; i++) {
-        let circle = `<div class="circle" id="board${i}" ></div>`;
+        let circle = $(`<div class="circle" id="board${i}" ></div>`);
         $('.fullboard').append(circle);
+        circle.on('click', function() {
+            console.log(this);
+            continueGame(this);
+        })
+        // console.log(circle);
     }
 
     for (let i = 1; i <= 44; i++) {
         let pegcircle = `<div class="pegcircle" id="peg${i}"></div>`;
         $('.pegs').append(pegcircle);
     }
+
 
     $('.fullboard').css('grid-template-rows', 'repeat(11, 1fr)');
     $('.fullboard').css('grid-template-columns', 'repeat(4, 1fr)');
@@ -66,105 +99,54 @@ $(document).ready(function () {
 
     $(".color").click(function () {
         let color = $(this).attr("id");
-        currentcolor = color;
+        currentColor = color;
 
         $(".currentcolor").css("background-color", color)
 
     });
 
 
-    const circleColorCreat = [];
+    // const circleColorCreat = [];
 
-    $(".circle").click(function () {
-        let getColor = $(this).attr("id");
+    // $(".circle").click(function () {
+    //  $(this).attr("id");
+    
+    //  $(this).css("background-color", currentColor);
+  
+    // });
 
-        if (isValid(getColor)) {
-            $(this).css("background-color", currentcolor);
+
+    $(".circle").dblclick(function(){
+        $(this).css("background-color", 'white');
+    })   
+
+    $(".btn-submit").click(function () {
+        if (userChoices.length !== 4) {
+            alert('fill out a row at least!');
+            return;
         }
 
-        // circleColorCreat.push(karbar);
-    });
-
-    $("button").click(function(){
-        // updatePegs();
-        checkWin();
-        changeCurrentRow();
-    });
-
-
-    function changeCurrentRow() {
-
-        currentRow -= 1;
-
-        var circleNumber = 4;
-
-        currentBoardCircles = [
-            "board" + ("currentRow" * circleNumber - 4),
-            "board" + ("currentRow" * circleNumber - 3),
-            "board" + ("currentRow" * circleNumber - 2),
-            "board" + ("currentRow" * circleNumber - 1),
-        ];
-
-        currentPegCircles = [
-            "peg" + ("currentRow" * circleNumber - 4),
-            "peg" + ("currentRow" * circleNumber - 3),
-            "peg" + ("currentRow" * circleNumber - 2),
-            "peg" + ("currentRow" * circleNumber - 1),
-        ];
-    }
-
-
-    function isValid(getColor) {
-
-        if (currentBoardCircles.includes(getColor) && hasWon === false) {
-            return true;
-        }
-        return false;
-    }
-
-    function checkWin() {
-
-        if(currentSecrets[0] === circle1Color &&
-            currentSecrets[1] === circle2Color &&
-            currentPegCircles[2] === circle3Color &&
-            currentSecrets[3] === circle4Color){
-                hasWon = true;
-                alert("Congratulation, You have Won\nThe code will now be displayed");
-                $(".color1").css("background-color", currentSecrets[0]);
-                $(".color2").css("background-color", currentSecrets[1]);
-                $(".color3").css("background-color", currentSecrets[2]);
-                $(".color4").css("background-color", currentSecrets[3]);
+        // compare
+        let flag = false;
+        userChoices.forEach((item, index) => {
+            if (item !== currentSecrets[index]) {
+                flag = true;
             }
+        })
 
-            return hasWon;
+        console.log(flag);
 
-    }
+        // userChoices = 
 
-    // var codeColor = $(".secretcolor.color1");
-    //   var backColor = codeColor.css("background-color");
-    //   console.log(backColor)
+        $('.secret-cover').css('display', 'none')
 
-    // var circleColor = $(".circle.41");
-    // var backColor2 = circleColor.css("background-color");
-    // console.log(backColor2)
+        if (!flag) {
+            alert('win');
+        } else {
+            alert('failed');
+        }
 
-    // $("button").click(function () {
-
-    // let color1 = $(".secretcolor.color1").css("background-color", circleColorCreat[0]);
-    // let color2 = $('.color2').css("background", secret);
-    // let color3 = $('.color3').css("background", secret);
-    // let color4 = $('.color4').css("background", secret);
-
-    //     console.log(color1)
-
-    //     if (backColor2 == currentSecrets[0]) {
-
-    //         $(".pegcircle.41").css("background-color", "black")
-    //     }
-
-
-
-    // })
+    })
 
 })
 
